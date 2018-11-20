fa46d6c8c13729e6a8dde361cb83bfbc151ede2e
Daniel Lopes <danielllopes11@gmail.com>
2018-11-19 23:25:53 -0200

    Prática Django ModelForm

--- pirates/views.py
+++ pirates/views.py
@@ -1,2 +1,46 @@
 from django.shortcuts import render
 from django.views import View
+from django.db.models import F, ExpressionWrapper, DecimalField, Sum
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from pirates.models import Tesouro
+
+class TesouroForm(forms.ModelForm):
+    class Meta:
+        model = Tesouro
+        fields = ['nome', 'quantidade', 'preco', 'img_tesouro']
+        labels = {
+            'nome': 'Nome',
+            'quantidade': 'Quantidade',
+            'preco': 'Preço',
+            'img_tesouro': 'Imagem'
+        }
+
+class ListaTesourosView(View):
+  def get(self, request):
+    return render(request, 'lista_tesouros.html',\
+        {'lista_tesouros': Tesouro.objects.annotate(valor_total=ExpressionWrapper(F('preco')*F('quantidade'),\
+                            output_field=DecimalField(max_digits=10, decimal_places=2, blank=True))),
+         'total_geral': Tesouro.objects.aggregate(total_geral=Sum(ExpressionWrapper(F('preco')*F('quantidade'),\
+                            output_field=DecimalField(max_digits=10, decimal_places=2, blank=True))))['total_geral']})
+
+class SalvarTesouro(View):
+    def get(self, request, id=None):
+        tesouro = Tesouro.objects.get(pk=id) if id != None else None
+        return render(request, 'salvar_tesouro.html', {'tesouro': TesouroForm(instance=tesouro)})
+    
+    def post(self, request, id=None):
+        tesouro = Tesouro.objects.get(pk=id) if id != None else None
+        form = TesouroForm(request.POST, request.FILES, instance=tesouro)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('lista_tesouros'))
+        else:
+            return render(request, 'salvar_tesouro.html', {'tesouro': form})
+
+class RemoverTesouro(View):
+    def get(self, request, id):
+        Tesouro.objects.get(pk=id).delete()
+        return HttpResponseRedirect(reverse('lista_tesouros'))
--- web_pirates/urls.py
+++ web_pirates/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from pirates.views import ListaTesourosView, SalvarTesouro, RemoverTesouro
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('', ListaTesourosView.as_view(), name='lista_tesouros'),
+    path('new', SalvarTesouro.as_view(), name='salvar_tesouro'),
+    path('new/<int:id>', SalvarTesouro.as_view(), name='salvar_tesouro'),
+    path('delete/<int:id>/', RemoverTesouro.as_view(), name='remover_tesouro'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
